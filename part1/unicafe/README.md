0d11394acf56b59951a46a3c2eff9b770354ef70
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-01-01 14:56:06 +0300

    part1: complete exercises 1.7-1.11 (unicafe)

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -1,1 +1,21 @@
 # Full Stack Open Part 1-2 Unicafe
+
+This web application collects customer feedback with three options: **good**, **neutral**, **bad**.
+
+The application displays: 
+- Total number of feedbacks
+- Average score
+- Percentage of positive feedback
+
+## Application Setup
+
+Follow those command below:
+
+```bash
+# Install dependencies
+npm install
+# Run the Application
+npm run dev
+```
+
+Then you can access the app at: http://localhost:5173/
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,39 +1,78 @@
 import { useState } from "react";
 
+const Headers = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+};
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+};
+
+// A separate component for statistics
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = 100 / (total / good);
+  // if there are no statistics, we won't display any of them 
+  if(total === 0){
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+      <table>
+        <tbody>
+          <StatisticLine  text="good" value={good}/>
+          <StatisticLine  text="bad" value={bad}/>
+          <StatisticLine  text="neutral" value={neutral}/>
+          <StatisticLine  text="all" value={total}/>
+          <StatisticLine  text="average" value={average}/>
+          <StatisticLine  text="positive" value={positive}/>
+        </tbody>
+      </table>
+  )
+};
+
+const StatisticLine = ({text, value}) => {
+  // Add '%' for statistic of positive 
+  if(text === "positive"){
+    return (
+      <tr>
+          <td>{text}</td>
+          <td>{value} %</td>
+      </tr>
+  )
+  }
+  return (
+    <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+    </tr>
+  )
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
 
-  const setToGood = () => {
-    const updatedGood = good + 1;
-    setGood(updatedGood);
-    setTotal(updatedGood + neutral + bad);
-  };
-  const setToNeutral = () => {
-    const updatedNeutral = neutral + 1;
-    setNeutral(updatedNeutral);
-    setTotal(good + updatedNeutral + bad);
-  };
-  const setToBad = () => {
-    const updatedBad = bad + 1;
-    setBad(updatedBad);
-    setTotal(good + neutral + updatedBad);
-  };
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={setToGood}>good</button>
-      <button onClick={setToNeutral}>neutral</button>
-      <button onClick={setToBad}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>Total {total}</p>
+      <Headers text="give feedback" />
+      <Button onClick={handleGoodClick} text="good"/>
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad"/>
+      <Headers text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 };
