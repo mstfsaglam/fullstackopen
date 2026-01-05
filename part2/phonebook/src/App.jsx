51fa28e4518a9a7dc3fee2b393e167874c81b93e
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-01-05 18:59:24 +0300

    feat: implement phonebook filtering and add number field

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,44 +1,101 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const isNameExists = persons.some(person => person.name === newName.trim());
-
-    if (!isNameExists) {
-      setPersons(persons.concat({name: newName}))
-    } else {
-      alert(`${newName} is already added to phonebook`)
-    }
-
-    setNewName('');
-  }
-
-  const handleInputChange = (event) => {
-    setNewName(event.target.value);
-
-  }
+ const Filtered = ({handleFilterInput, filter}) => {
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleFormSubmit}>
+        filter shown with <input value={filter} onChange={handleFilterInput}/>
+      </div>
+  )
+ }
+
+ const PersonsForm = ({handleFormSubmit, newName, handleNameInput, newNumber, handleNumberInput}) => {
+  
+  return (
+    <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input value={newName} onChange={handleInputChange}/>
+          name: <input value={newName} onChange={handleNameInput}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-        {persons.map(person => 
-        <p key={person.name}>
-          {person.name}
-        </p>)}  
+  )
+ }
+
+  const Persons = ({showPersons}) => {
+    return (
+      <div>
+        {showPersons.map(person => 
+        <p key={person.id}>
+          {person.name + ' '}
+          {person.number}
+        </p>)} 
+      </div>
+    )
+  }
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Checks if new person is in same name at persons list
+    const isNameExists = persons.some(person => person.name === newName.trim());
+
+    if (!isNameExists) {
+      const newPerson = persons.concat({
+          name: newName.trim(),
+          number: newNumber.trim(),
+          id: persons.length === 0 ? persons.length + 1 : Math.max(...persons.map(person => person.id)) + 1
+        })
+      setPersons(newPerson)
+    }
+    else {
+      alert(`${newName} is already added to phonebook`)
+    }
+
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const handleNameInput = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleFilterInput = (event) => {
+    setFilter(event.target.value)
+
+  }
+  //This is filtering persons data and displays them
+  const cleanFilterInput = filter.trim().toLowerCase();
+  const filteredPersonsList = persons.filter(person =>  person.name.toLowerCase().includes(cleanFilterInput));      
+  const showPersons = filter ? filteredPersonsList: persons;
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filtered filter={filter} handleFilterInput={handleFilterInput}/>
+      <h3>add a new</h3>
+      <PersonsForm 
+        handleFormSubmit={handleFormSubmit}
+        newName={newName} handleNameInput={handleNameInput}
+        newNumber={newNumber} handleNumberInput={handleNumberInput}
+      />
+      <h3>Numbers</h3>
+      <Persons showPersons={showPersons}/>
     </div>
   )
 }
