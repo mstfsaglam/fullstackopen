3012cff7aa5020dc90a4a24bb9c8bfa85a8ad9a1
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-02-07 14:39:49 +0300

    refactor: separate files and logs on blog app

---
+++ part4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const { MONGODB_URI } = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+logger.info('connection to ', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info("Connected to database")
+  })
+  .catch(error => {
+    logger.info("Error connection to database", error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,39 +1,7 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
