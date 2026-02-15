6f79fa995304dbff60d016cb8c9d38b267ac5e57
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-02-15 22:28:56 +0300

    refactor: describe tests in groups

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -8,174 +8,123 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
-
-describe('blogs test for each property', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-      __v: 0
-    }
-  ]
-
+describe('When there is initially blogs saved', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(listWithOneBlog)
+    await Blog.insertMany(listHelper.blogList)
   })
 
-  test('total likes on blogs', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 36)
-  })
+  describe('After some properties change', () => {
+    test('total likes on blogs', () => {
+      const result = listHelper.totalLikes(listHelper.blogList)
+      assert.strictEqual(result, 36)
+    })
 
-  test('favorite likes on blogs', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
-    assert.deepStrictEqual(result, {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
+    test('favorite likes on blogs', () => {
+      const result = listHelper.favoriteBlog(listHelper.blogList)
+      assert.deepStrictEqual(result, {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+      })
+    })
+
+    test('Most blogs in Authors', () => {
+      const result = listHelper.mostBlogs(listHelper.blogList)
+      assert.deepStrictEqual(result, {
+        author: 'Robert C. Martin',
+        blogs: 3
+      })
+    })
+
+    test('Most likes in Authors', () => {
+      const result = listHelper.mostLikes(listHelper.blogList)
+      assert.deepStrictEqual(result, {
+        author: 'Edsger W. Dijkstra',
+        likes: 17
+      })
     })
   })
 
-  test('Most blogs in Authors', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog)
-    assert.deepStrictEqual(result, {
-      author: 'Robert C. Martin',
-      blogs: 3
+  describe('Getting blogs and control property names', () => {
+    test('Get all blogs from database', async () => {
+      let blogs = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(blogs.body.length, 6)
+    })
+
+    test('Unique identifier property name', async () => {
+      const blogs = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      blogs.body.forEach((blog, index) => {
+        assert.ok(blog.id, `blog at index ${index} is missing id`)
+      })
     })
   })
 
-  test('Most likes in Authors', () => {
-    const result = listHelper.mostLikes(listWithOneBlog)
-    assert.deepStrictEqual(result, {
-      author: 'Edsger W. Dijkstra',
-      likes: 17
+  describe('Check creating new blogs and exist properties', () => {
+    test('Creating new blog', async () => {
+      const newBlog = {
+        title: 'testing create blog',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 22,
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await Blog.find({})
+      assert.strictEqual(response.length, listHelper.blogList.length + 1)
+
+      const titles = response.map(blog => blog.title)
+      assert.ok(titles.includes('testing create blog'), true)
     })
-  })
 
-  test('Get all blogs from database', async () => {
-    let blogs = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+    test('are likes exist in blog', async () => {
+      const newBlog = {
+        title: 'are likes exist',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      }
 
-    assert.strictEqual(blogs.body.length, 6)
-  })
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  test('Unique identifier property name', async () => {
-    const blogs = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    blogs.body.forEach((blog, index) => {
-      assert.ok(blog.id, `blog at index ${index} is missing id`)
+      const response = await Blog.find({})
+      const createdBlog = response.find(blog => blog.title === 'are likes exist')
+      assert.strictEqual(createdBlog.likes, 0)
     })
-  })
-  test('test for creating new blog', async () => {
-    const newBlog = {
-      title: 'testing create blog',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 22,
-    }
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
 
-    const response = await Blog.find({})
-    assert.strictEqual(response.length, listWithOneBlog.length + 1)
+    test('are title and URL exist in blog', async () => {
+      const newBlog = {
+        author: 'Edsger W. Dijkstra',
+        likes: 10,
+      }
 
-    const titles = response.map(blog => blog.title)
-    assert.ok(titles.includes('testing create blog'), true)
-  })
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-  test('are likes exist in blog', async () => {
-    const newBlog = {
-      title: 'are likes exist',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await Blog.find({})
-    const createdBlog = response.find(blog => blog.title === 'are likes exist')
-    assert.strictEqual(createdBlog.likes, 0)
-  })
-
-  test('are title and URL exist in blog', async () => {
-    const newBlog = {
-      author: 'Edsger W. Dijkstra',
-      likes: 10,
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    const blogs = await Blog.find({})
-    assert.strictEqual(blogs.length, listWithOneBlog.length)
+      const blogs = await Blog.find({})
+      assert.strictEqual(blogs.length, listHelper.blogList.length)
+    })
   })
 
   after(async () => {
