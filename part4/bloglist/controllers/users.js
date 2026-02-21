e1d178b4fb093faff7edfef656562b48bedbb2fa
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-02-22 00:48:59 +0300

    feat: add validation than populate user and blog content

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,12 +3,16 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs')
   response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'invalid password' })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
