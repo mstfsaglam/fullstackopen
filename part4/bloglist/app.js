e6e6c09d5be95252b6d643482f03faa34d55f645
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-02-24 01:00:38 +0300

    refactor: get token and replace it in middleware instead of controllers

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -21,6 +21,7 @@ mongoose.connect(MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
