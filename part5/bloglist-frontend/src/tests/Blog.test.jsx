0f1902caf77cbf6cce43525f8b4b7d8dd0af600d
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-04-06 23:39:25 +0300

    feat: add test for shown url and likes with click view button

--- part5/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
 test('renders blog title and blog author', () => {
@@ -13,4 +14,33 @@ test('renders blog title and blog author', () => {
   const authorElement = screen.getByText(/user/)
   expect(titleElement).toBeDefined()
   expect(authorElement).toBeDefined()
+})
+
+test('url and likes are shown when view button is clicked', async () => {
+  const blog = {
+    title: 'second test',
+    author: 'admin',
+    url: 'https://test.com',
+    likes: 0,
+    user: {
+      name: 'user for test',
+      username: 'admin'
+    }
+  }
+
+  const testUser = {
+    username: 'admin'
+  }
+
+  render(<Blog blog={blog} user={testUser}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const urlElement = await screen.findByText(/https:\/\/test.com/)
+  const likesElement = screen.getByText(/likes 0/)
+
+  expect(urlElement).toBeVisible()
+  expect(likesElement).toBeVisible()
 })
